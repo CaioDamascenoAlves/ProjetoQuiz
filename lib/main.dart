337aed0e5ccae4dd1f628e156571d90f4d46b320
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2022-07-10 20:03:17 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
+
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      {
+        'texto': 'Qual é sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é a seu animal favorito?',
+        'respostas': ['Coala', 'Lemore', 'Elefante', 'Raposa'],
+      },
+      {
+        'texto': 'Qual seu instrutor favorito?',
+        'respostas': ['Leticia', 'Lucas', 'Rafael', 'Gabriel']
+      }
+    ];
+
+    List<String> respostas =
+        perguntas[_perguntaSelecionada].cast()['respostas'];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          shadowColor: Color(8),
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas.map((t) => Resposta(t, _responder)).toList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
---
+++ lib/questao.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: dead_code
+
+class Questao extends StatelessWidget {
+  final String texto;
+
+  Questao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        texto,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
